13fd778a923cde18445366242bd8260c01baef83
Davi Cabral <davidasilvacabral315@gmail.com>
2024-04-22 15:08:00 -0300

    att

---
+++ java.js/java.js
@@ -0,0 +1,19 @@
+function claro(){
+    document.body.style.backgroundColor = "#ffd8ae"
+    document.body.style.color = "black"
+
+    var btEs = document.getElementById('escuro')
+    var btCla = document.getElementById('claro')
+
+    
+}
+
+function escuro(){
+   document.body.style.backgroundColor ="#333333"
+   document.body.style.color = "white"
+
+   var btEs = document.getElementById('escuro')
+   var btCla = document.getElementById('claro')
+
+   
+}
